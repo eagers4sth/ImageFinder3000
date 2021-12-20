08e891879d3179d10738bd05106b98ee0d2df08b
KiruxaLight <kirillworkraft@yandex.ru>
2021-12-20 14:41:05 +0300

    'imutils.py'

---
+++ imutils.py
@@ -0,0 +1,19 @@
+import numpy as np
+from PIL import Image
+import imagehash
+import numpy
+
+def get_hash(img: Image):
+    hash_image = imagehash.average_hash(img)
+    return hash_image
+
+def read_image(name: str):
+    img = Image.open(name)
+    my_hash = get_hash(img)
+    img = np.asarray(img)
+    return img, my_hash
+
+one, two = read_image("jelly1.png")
+
+print(two)
+print(one)
