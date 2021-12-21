8dbc057a636c4edf96ace5f17b37275e938b43d4
GolubevVA <89020432+GolubevVA@users.noreply.github.com>
2021-12-21 12:37:14 +0300

    рабочие стикеры и images

--- main.py
+++ main.py
@@ -2,25 +2,19 @@ import logging
 import os
 import tempfile
 
+import telegram.ext
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from search import search
 from imutils import read_image
 from imutils import get_text_from_image
-from PIL import Image
-import imagehash
-import numpy
+from image_info import ImageInfo
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
-
-class ImageInfo:
-    def __init__(self, my_hash, my_text):
-        hash_ = my_hash
-        text = my_text
 
 def start(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
@@ -34,22 +28,43 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Help!\n/search <text> to find the image\n')
 
 def make_text_command(update: Update, context: CallbackContext) -> None:
+    logging.info("I see a picture")
     picture = update.message.photo[-1]
     file = picture.get_file()
-    #update.message.reply_text("Я бы сохранил, если бы мог")
+    logging.info("received an image")
     with tempfile.TemporaryDirectory() as d:
         file_path = os.path.join(d, "image.png")
         file.download(file_path)
-        #update.message.reply_text("Я сохранил изображение в" + file_path)
-        image, hash_ = read_image(file_path, context.chat_data, update.message.message_id)
+        logging.info("saved the image")
+        image, hash_ = read_image(file_path)
         context.chat_data[update.message.message_id] = ImageInfo(hash_, get_text_from_image(file_path, image))
 
+def sticker_to_text_command(update: Update, context: CallbackContext) -> None:
+    logging.info("I see a sticker")
+    stick = update.message.sticker
+    file = stick.get_file()
+    logging.info("received a sticker")
+    with tempfile.TemporaryDirectory() as d:
+        file_path = os.path.join(d, "image.png")
+        file.download(file_path)
+        logging.info("saved the stciker")
+        image, hash_ = read_image(file_path)
+        context.chat_data[update.message.message_id] = ImageInfo(hash_, get_text_from_image(file_path, image))
+        logging.info(len(context.chat_data))
+
 def search_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_to_message(search(context.args, context.chat_data))
+    if not context.chat_data:
+        update.message.reply_text('No photo data')
+        return
+    logging.info("going to find an image")
+    logging.info(len(context.chat_data))
+    logging.info(' '.join(context.args))
+    context.bot.send_message(chat_id=update.message.chat_id, text='image', reply_to_message_id=search(' '.join(context.args), context.chat_data))
 
 def main() -> None:
     """Start the bot."""
-    updater = Updater("5074946865:AAFUXJ20UVO618nGVUPqk3xr2yfBvQ6tKE0")
+    my_persistence = telegram.ext.PicklePersistence(filename='data.pickle')
+    updater = Updater("5074946865:AAFUXJ20UVO618nGVUPqk3xr2yfBvQ6tKE0", persistence=my_persistence, use_context=True)
 
     dispatcher = updater.dispatcher
 
@@ -57,12 +72,11 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(CommandHandler("search", search_command))
 
-    #dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
     dispatcher.add_handler(MessageHandler(Filters.photo & ~Filters.command, make_text_command))
+    dispatcher.add_handler(MessageHandler(Filters.sticker & ~Filters.command, sticker_to_text_command))
     updater.start_polling()
 
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
