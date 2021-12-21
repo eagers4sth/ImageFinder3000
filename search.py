6453b5b145b2480c40e33a72e68b293bf24bcbcc
vladislava <eagers4sth>
2021-12-21 10:44:18 +0300

    Updated search and ImageInfo

--- search.py
+++ search.py
@@ -1,16 +1,16 @@
-
 from difflib import SequenceMatcher
+from image_info import ImageInfo
 
 def search(text: str, database: dict) -> None:
     """Returns the most common to text image id"""
     return max([
-        (SequenceMatcher(None, database[element][0], text).ratio(), database[element][0]) 
+        (SequenceMatcher(None, database[element].text, text).ratio(), element) 
         for element in database
     ])[1]
 
 
 if __name__ == '__main__':
     database = dict()
-    database[0] = ("string1", "h1")
-    database[1] = ("string2", "h2")
+    database[6] = ImageInfo(hash ="h12", text="string1")
+    database[4] = ImageInfo(hash ="h22", text="string2")
     print(search("string31", database))
